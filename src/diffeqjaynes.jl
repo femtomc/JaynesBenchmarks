08780a9730bc99d89a5a2c896286d1f9194078b7
McCoy R. Becker <mccoybecker@gmail.com>
2020-07-23 19:34:20 -0700

    Working on DiffEq integration.

---
+++ src/diffeqjaynes.jl
@@ -0,0 +1,46 @@
+module DiffEqJaynes
+
+using Jaynes
+using DifferentialEquations
+
+# For adjoints.
+using DiffEqSensitivity
+
+# Generate some data with fixed parameters.
+function lotka_volterra(du,u,p,t)
+  x, y = u
+  α, β, δ, γ = p
+  du[1] = dx = (α - β*y)x
+  du[2] = dy = (δ*x - γ)y
+end
+p = [1.5, 1.0, 3.0, 1.0]
+u0 = [1.0,1.0]
+prob = ODEProblem(lotka_volterra,u0,(0.0,10.0),p)
+data = Array(solve(prob,Tsit5(),saveat=0.1))
+
+function fit_lotka_volterra()
+    σ = rand(:σ, InverseGamma(2, 3))
+    α = rand(:α, truncated(Normal(1.5, 0.5), 0.5, 2.5))
+    β  = rand(:β, truncated(Normal(1.2, 0.5), 0, 2))
+    γ = rand(:γ, truncated(Normal(3.0, 0.5), 1, 4))
+    δ = rand(:δ, truncated(Normal(1.0, 0.5), 0, 2))
+
+    p = [α, β, γ, δ]
+    prob = ODEProblem(lotka_volterra, u0, (0.0, 10.0), p)
+    predicted = solve(prob, Tsit5(), saveat=0.1)
+
+    for i = 1:length(predicted)
+        rand(:obs => i, MvNormal(predicted[i], σ))
+    end
+end
+
+ret, cl = simulate(fit_lotka_volterra)
+display(cl.trace)
+
+# Inference.
+sel = selection([(:α, ), (:β, ), (:γ, ), (:σ, ), (:δ, )])
+
+new, acc = hmc(sel, cl)
+@time new, acc = hmc(sel, cl)
+
+end # module
--- src/hmmpf_gen.jl
+++ src/hmmpf_gen.jl
@@ -23,13 +23,14 @@ end
 
 # Note - off-by-one difference for markov specialize vs. Gen - try and fix later, I think it's irrelevant from a performance perspective.
 j_simulation = obs -> begin
-    init_obs = selection((:z0, 0.0))
+    init_obs = selection([(:z0, ) => 0.0])
     ps = initialize_filter(init_obs, 1000, kernelize, (1, ))
     for i in 2 : length(obs)
-        sel = selection((:k => i => :x, obs[i - 1]))
+        sel = selection([(:k, i, :x) => obs[i - 1]])
 
         # Complexity of filter step is constant as a size of the trace.
         filter_step!(sel, ps, Jaynes.NoChange(), (i,))
+        check_ess_resample!(ps)
     end
     return ps
 end
@@ -56,7 +57,7 @@ chain = Gen.Unfold(g_kernel)
     z_0 = @trace(normal(0.0, 1.0), :z0)
     init_state = State(z_0, 0.0)
     states = @trace(chain(t, init_state), :chain)
-    return [init_state, states...]
+    return (init_state, states)
 end
 
 Gen.load_generated_functions()
@@ -66,9 +67,11 @@ simulation = obs -> begin
     ps = initialize_particle_filter(g_sim, (0, ), init_obs, 1000)
     for t in 1 : length(obs) - 1
         sel = Gen.choicemap((:chain => t => :x, obs[t]))
+        #sel = Gen.choicemap()
 
         # Complexity of filter step is constant as a size of the trace.
         Gen.particle_filter_step!(ps, (t,), (UnknownChange(),), sel)
+        Gen.maybe_resample!(ps)
     end
     return ps
 end
@@ -77,24 +80,31 @@ end
 function benchmark(t::Int)
     # Some set of observations.
     obs = [rand() for i in 1 : t]
-    
-    simulation(obs)
+
     t_start = time_ns()
     ps = simulation(obs)
     g_time = (time_ns() - t_start) / 1e9
     g_lmle = log_ml_estimate(ps)
-   
-    j_simulation(obs)
+
     t_start = time_ns()
     ps = j_simulation(obs)
     j_time = (time_ns() - t_start) / 1e9
-    j_lmle = ps.lmle
+    j_lmle = get_lmle(ps)
     g_time, j_time, g_lmle, j_lmle
 end
 
-steps = [1, 3, 10, 30, 50, 100, 150, 200, 500, 800]
+#steps = collect(1 : 5 : 500) .+ 4
+#prepend!(steps, 1)
+steps = [1, 5, 10, 20, 50, 100, 200, 500, 800, 1000, 1500]
+
+# Warm up.
+obs = [rand() for i in 1 : 10]
+simulation(obs)
+j_simulation(obs)
+
 times = map(steps) do t
-    benchmark(t)
+    println("T -> $t")
+    @time benchmark(t)
 end
 
 println(times)
